63ccc2c4a96bddd8a6e7cc99a6ae48000a5fe243
Serhat Yurdakul <serhatyurdakulsl@gmail.com>
2024-03-23 18:48:42 +0300

    Add todo page completed

--- TodoApp/src/app/pages/todo-add/todo-add.component.ts
+++ TodoApp/src/app/pages/todo-add/todo-add.component.ts
@@ -1,12 +1,27 @@
+import { Router } from '@angular/router';
+import { TodoService } from './../../services/todo.service';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TodoAdd } from '../../models/todo-add';
 
 @Component({
   selector: 'app-todo-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-add.component.html',
-  styleUrl: './todo-add.component.css'
+  styleUrl: './todo-add.component.css',
 })
 export class TodoAddComponent {
+  todoForm = new FormGroup({
+    content: new FormControl(''),
+  });
 
+  constructor(private todoService: TodoService, private router: Router) {}
+  add() {
+    this.todoService.add(this.todoForm.value as TodoAdd).subscribe((x) => {
+      if (x == true) {
+        this.router.navigateByUrl('/todos');
+      }
+    });
+  }
 }
